1ceb208bf17beac20266bebda8eb7927d897e708
tony wilson <40241323+tjmode@users.noreply.github.com>
2019-10-09 16:47:11 +0530

    commited!

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import speech_recognition as sr
-r=sr.Recognizer()
-with sr.Microphone() as source:
-	print("hello say any anthing")
-	audio=r.listen(source)
-	try:
-		text=r.recognize_google(audio)
-		print("you sai:{}".format(text))
-	except:
-		print("sorry")
+import pyttsx3
+from flask import Flask
+app=Flask(__name__)
+@app.route('/')
+def hemo():
+	engine=pyttsx3.init()
+	engine.say("hello dude developer")
+	engine.runAndWait()
+	r=sr.Recognizer()
+	with sr.Microphone() as source:
+		print("hello say any anthing")
+		audio=r.listen(source)
+		try:
+			text=r.recognize_google(audio)
+			print("you sai:{}".format(text))
+			engine=pyttsx3.init()
+			engine.say(text)
+			engine.runAndWait()
+		except:
+			engine=pyttsx3.init()
+			engine.say("oooluugaaa peesu")
+			engine.runAndWait()
+	return "say anything"
+if __name__ == '__main__':
+	app.run(debug=True)
